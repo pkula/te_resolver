5fc1d81333c0888e3773e93b36ffbaf985517408
Przemyslaw Kula <px.kula@gmail.com>
2024-03-12 08:41:38 +0100

    Add second part

--- main.py
+++ main.py
@@ -1,55 +1,69 @@
 import argparse
-import os
 import logging
-import pandas as pd
+import os
 from pathlib import Path
 
+import pandas as pd
+
+MAIN_PATH = "."
+
+TE_BLAST_IDENT_PRC = 90
+GENOME_BLAST_IDENT_PRC = 90
+
+TE_IN = ""
+ONT_IN = ""
+GENOME_IN = ""
 
 class Blast:
     def __init__(self):
         self.config = Config()
 
-    # blast for sequence Alex1_Alex3
+    @staticmethod
+    def _get_fasta_files(directory):
+        return [a for a in Path(directory).glob("*.fasta")] + [
+            a for a in Path(directory).glob("*.fa")
+        ]
 
-    def make_db(self):
+    def make_pass_db(self):
         logging.info("start make db")
-        # 1) make a database from your nucleotide sequences.
         pass_path = self.config.pass_reads_merged_path
-        os.system(
-            f"makeblastdb -in {pass_path}/LK5_1_3.fasta -dbtype nucl -out {pass_path}/LLK5_1_3"
-        )
-        os.system(
-            f"makeblastdb -in {pass_path}/K10B.fasta  -dbtype nucl -out {pass_path}/K10B"
-        )
-        os.system(
-            f"makeblastdb -in {pass_path}/K10f.fasta  -dbtype nucl -out {pass_path}/K10f"
-        )
+
+        for f in self._get_fasta_files(pass_path):
+            name = pass_path / f.name.split(".")[0]
+            os.system(f"makeblastdb -in {f} -dbtype nucl -out {name}")
         logging.info("end make db")
 
-    def run_blast(self):
+    def _run_blast(self, path, query, db, out):
+        cmd = (
+            f"blastn -num_threads 20 -outfmt 6 -query {query} "
+            f"-db {db} -out {out} -dust no -perc_identity {TE_BLAST_IDENT_PRC/100}"
+        )
+        os.system(f"cd {path} && {cmd}")
+
+    def run_pass_blast(self):
         logging.info("run blast")
 
-        main_path = self.config.main_path
-        analysis_path = self.config.analysis_path
+        for f in self._get_fasta_files(self.config.pass_reads_merged_path):
+            name = f.name.split(".")[0]
+            name_path = self.config.pass_reads_merged_path / name
+            te_ont_path = self.config.blast_path / f"TE_{name}.bl"
+            # todo delete Alex1_Alex3.fasta
+            cmd = (
+                f"blastn -num_threads 20 -outfmt 6 -query /home/kira/projects/src/results/other_data/Alex1_Alex3.fasta "
+                f"-db {name_path} -out {te_ont_path} -dust no -perc_identity {TE_BLAST_IDENT_PRC/100}"
+            )
+            os.system(cmd)
 
-        # 2) run blast https://www.ncbi.nlm.nih.gov/books/NBK279684/) from /home/MSA1/username
         # todo k value
-        cmd = (
-            "blastn -num_threads 20 -outfmt 6 -query results/other_data/Alex1_Alex3.fasta "
-            "-db results/pass_reads_merged/LK5_1_3 -out analysis/blast/Alex_LK5_1_3.bl -dust no -perc_identity 0.9"
-        )
-        os.system(f"cd {main_path} && {cmd}")
-        cmd = (
-            "blastn -num_threads 20 -outfmt 6 -query results/other_data/Alex1_Alex3.fasta "
-            "-db results/pass_reads_merged/K10B -out analysis/blast/Alex_K10B.bl -dust no -perc_identity 0.9"
-        )
-        os.system(f"cd {main_path} && {cmd}")
-        cmd = (
-            "blastn -num_threads 20 -outfmt 6 -query results/other_data/Alex1_Alex3.fasta "
-            "-db results/pass_reads_merged/K10f -out analysis/blast/Alex_K10f.bl -dust no -perc_identity 0.9"
-        )
-        os.system(f"cd {main_path} && {cmd}")
+        # todo _run_blast
         logging.info("end run blast")
+
+    # todo
+    def run_genome_blast(self):
+        cmd = f"makeblastdb -in {self.config.main_path}/genome/DCARv3.4.fa  -dbtype nucl -out {self.config.main_path}/genome/DCARv3.4"
+        os.system(cmd)
+        cmd = f"blastn -num_threads 20 -outfmt 6 -query {self.config.pass_reads_merged_path}/TE_K10B.masked.fasta -db {self.config.genome_path}/DCARv3.4 -out {self.config.blast_path}/TE_K10B_genome.bl -perc_identity {TE_BLAST_IDENT_PRC/100} -max_target_seqs 4"
+        os.system(cmd)
 
 
 class Config:
@@ -58,68 +72,49 @@ class Config:
             cls.instance = super(Config, cls).__new__(cls)
         return cls.instance
 
-    def __init__(self):
-        logging.info("start config")
-        # prepare parser
+    def prepare_parser(self):
         parser = argparse.ArgumentParser(description="TE pipeline resolver")
 
         parser.add_argument("-w", action="store_true", help="flag default false")
         parser.add_argument("-k", action="store", type=int, default=90, help="")
         parser.add_argument("-v", "--verbose", action="store_true")
+        return parser
 
-        self.path = Path(".").absolute()  # /home/username/projects/te_resolver
+    def __init__(self):
+        logging.info("start config")
+        parser = self.prepare_parser()
+        self.args = parser.parse_args()
 
         # set needed paths
+        self.main_path = self.path = Path(MAIN_PATH).absolute() / "src"
 
-        # /home/username/projects
-        self.main_path = self.path.parent  # m main path -> analysis and results
-        self.results_path = (
-            self.main_path / "results"
-        )  # /home/username/projects/results
-        self.other_data_path = (
-            self.results_path / "other_data"
-        )  # /home/username/projects/results/other_data
-        self.pass_reads_merged_path = (
-            self.results_path / "pass_reads_merged"
-        )  # /home/username/projects/results/pass_reads_merged
+        self.results_path = self.main_path / "results"
+        self.other_data_path = self.results_path / "other_data"
+        self.pass_reads_merged_path = self.results_path / "pass_reads_merged"
 
-        self.analysis_path = (
-            self.main_path / "analysis"
-        )  # /home/username/projects/analysis
-        self.blast_path = (
-            self.analysis_path / "blast"
-        )  # /home/username/projects/analysis
+        self.analysis_path = self.main_path / "analysis"
+        self.blast_path = self.analysis_path / "blast"
 
+        self.genome_path = self.main_path / "genome"
 
+        # Long reads from ONT sequencing files/directories -o
+        self.raw_ont_path = Path("/home/kira/data2/ont")
+        # Transcposon sequences (TE) files/directories -t
+        self.raw_te_path = Path("/home/kira/data2/te")
+        # Reference genome files/directories -g
+        self.raw_genome_path = Path("/home/kira/data2/genome")
 
+        self.create_directories()
+        logging.info("end config")
 
-        # create needed paths
-
-
-
-        # mkdir m paths
+    def create_directories(self):
         self.main_path.mkdir(parents=True, exist_ok=True)
         self.results_path.mkdir(parents=True, exist_ok=True)
         self.other_data_path.mkdir(parents=True, exist_ok=True)
         self.pass_reads_merged_path.mkdir(parents=True, exist_ok=True)
         self.analysis_path.mkdir(parents=True, exist_ok=True)
         self.blast_path.mkdir(parents=True, exist_ok=True)
-
-
-
-
-
-
-        self.args = parser.parse_args()
-
-        # Long reads from ONT sequencing files/directories -o
-        self.ont_path = Path("/home/kira/data2/ont")
-        # Transcposon sequences (TE) files/directories -t
-        self.te_path = Path("/home/kira/data2/te")
-        # Reference genome files/directories -g
-        self.genome_path = Path("/home/kira/data2/genome")
-
-        logging.info("end config")
+        self.genome_path.mkdir(parents=True, exist_ok=True)
 
 
 class Runner:
@@ -127,44 +122,54 @@ class Runner:
         self.config = Config()
 
     @staticmethod
-    def from_fq_to_fa(fq, fa):
-        os.system(f"seqtk seq -a {fq} > {fa}")
+    def from_fq_to_fa(fq):
+        fq = Path(fq).absolute()
+        basename = Path(fq).name.split(".")[0]
+        os.system(f"seqtk seq -a {fq} > {fq.parent / basename}.fasta")
 
     @staticmethod
     def _copy_file(from_file, to_file):
         os.system(f"cp {from_file} {to_file}")
 
-    def preparation(self):
-        logging.info("start preparation")
+    def copy_files(self):
         """
         # for all sequencing results
         #cat  FAS82558_pass*.fastq.gz > LK5_1_3.fastq.gz
         """
-        other_data_path = self.config.other_data_path
-        pass_reads_merged_path = self.config.pass_reads_merged_path
         # copy fastq.gz files into pass_reads_merged
-        for f in self.config.ont_path.glob("*.fastq.gz"):
-            self._copy_file(f, pass_reads_merged_path)
+        logging.info("start copying fq ont")
+        for f in self.config.raw_ont_path.glob("*.fastq.gz"):
+            self._copy_file(f, self.config.pass_reads_merged_path)
+        logging.info("start copying fq ont")
+
         # upload Alex1_Alex3.fasta into other_data
-        for f in self.config.genome_path.glob("*.fasta"):
-            self._copy_file(f, other_data_path)
-        logging.info("end preparation")
+        logging.info("start copying te")
+        for f in self.config.raw_te_path.glob("*.fasta"):
+            self._copy_file(f, self.config.other_data_path)
+        logging.info("end copying te")
+
+        logging.info("start copying genome")
+        for f in self.config.raw_genome_path.glob("*"):
+            self._copy_file(f, self.config.genome_path)
+        logging.info("end copying te")
 
     def create_bed_files(self):
         """
+        for file in *.bl; do
         awk 'OFS="\t"  {
         if ($9 <$10)
-             print $2,$9,$10,$1;
+            print $2,$9,$10,$1;
         else
             print $2,$10,$9,$1;
         }' $file | sort -k1,1 -k2,2n |bedtools merge -i - -d 1500 -c 4 -o collapse  >$file.bed;
         done
         """
 
-        def xxx(row):
+        def sort_row(row):
             if row["sstart"] > row["send"]:
                 row["sstart"], row["send"] = row["send"], row["sstart"]
             return row
+
         column_names = [
             "qseqid",
             "sseqid",
@@ -182,65 +187,66 @@ class Runner:
 
         blast_path = self.config.blast_path
         for f in blast_path.glob("*.bl"):
-            
-            okno = pd.read_csv(
-                f"{f}", sep="\t", header=None, names=column_names
-            )
-            bed_path = str(blast_path / f"{f.name}.bed")
-            bed_path2 = str(blast_path / f"{f.name}.bed2")
-            okno.apply(xxx, axis=1).to_csv(
-                bed_path,
+            basename = Path(f.name).stem
+            merged_path = str(blast_path / f"{basename}.merged")
+            bed_path = str(blast_path / f"{f}.bed")
+
+            csv = pd.read_csv(f"{f}", sep="\t", header=None, names=column_names)
+            csv.apply(sort_row, axis=1).to_csv(
+                merged_path,
                 sep="\t",
                 header=None,
                 index=False,
                 columns=["sseqid", "sstart", "send", "qseqid"],
             )
             os.system(
-                f"sort -k1,1 -k2,2n {bed_path} >{bed_path2}"
-            )
-            merged_path = str(blast_path / f"{f.name}.merged")
-            cmd = f"bedtools merge -i {bed_path2} -d 1500 -c 4 -o collapse >{merged_path}"
-            os.system(
-                f"bedtools merge -i {bed_path2} -d 1500 -c 4 -o collapse >{merged_path}"
+                f"cat {merged_path} | sort -k1,1 -k2,2n | bedtools merge -i - -d 1500 -c 4 -o collapse  > {bed_path}"
             )
 
+    # todo
+    def create_seq_list(self):
+        cmd = f"for file in {self.config.blast_path}/*.bed; do awk '{{print $1}}' $file|sort|uniq > $file.seq_list.txt; done "
+        os.system(cmd)
+        cmd = f"for file in {self.config.blast_path}/*.txt; do wc -l $file >> {self.config.blast_path}/count.seq_list; done "
+        os.system(cmd)
 
-    def preparation2(self):
-        logging.info("start preparation2")
+    # todo
+    def subseq(self):
+        for f in self.config.pass_reads_merged_path.glob("*.fasta"):
+            name = f.name.split(".")[0]
+            cmd = f"seqtk subseq {f} {self.config.blast_path}/TE_{name}.bl.bed.seq_list.txt >{self.config.pass_reads_merged_path}/TE_{name}.fasta"
+            os.system(cmd)
+
+    # todo
+    def mask(self):
+        cmd = f"bedtools maskfasta -fi {self.config.pass_reads_merged_path}/TE_K10B.fasta -bed {self.config.analysis_path}/blast/TE_K10B.bl.bed -fo {self.config.pass_reads_merged_path}/TE_K10B.masked.fasta"
+        os.system(cmd)
+
+    def convert_pass(self):
+        logging.info("start converting pass from fastaq to fasta")
         """
-        #fq.gz into fasta (for blast)
+        # change fq.gz into fasta (for blast)
         #seqtk you can install by: https://anaconda.org/bioconda/seqtk
-        seqtk seq -a LK5_1_3.fastq.gz > LK5_1_3.fasta
-        grep '>' LK5_1_3.fasta|wc -l #to display number of seq  1393171
         seqtk seq -a K10B.fastq.gz >K10B.fasta
         grep '>' K10B.fasta|wc -l #to display number of seq     6939343
-        seqtk seq -a K10f.fastq.gz >K10f.fasta
-        grep '>' K10f.fasta|wc -l #to display number of seq     4775182
         """
         pass_path = self.config.pass_reads_merged_path
-        fq1 = pass_path / "LK5_1_3.fastq.gz"
-        fa1 = pass_path / "LK5_1_3.fasta"
-        self.from_fq_to_fa(fq1, fa1)
-        fq2 = pass_path / "K10B.fastq.gz"
-        fa2 = pass_path / "K10B.fasta"
-        self.from_fq_to_fa(fq2, fa2)
-        fq3 = pass_path / "K10f.fastq.gz"
-        fa3 = pass_path / "K10f.fasta"
-        self.from_fq_to_fa(fq3, fa3)
-
-        logging.info("end preparation2")
+        for f in pass_path.glob("*.fastq.gz"):
+            self.from_fq_to_fa(f)
+        logging.info("end converting pass to fasta")
 
 
 if __name__ == "__main__":
     runner = Runner()
-    runner.preparation()
-    runner.preparation2()
     blast = Blast()
-    blast.make_db()
-    blast.run_blast()
+    runner.copy_files()
+    runner.convert_pass()
+    blast.make_pass_db()
+    blast.run_pass_blast()
+    runner.create_bed_files()
+    runner.create_seq_list()
+    runner.subseq()
+    runner.mask()
+    blast.run_genome_blast()
 
-    #runner.create_bed_files()
-    import ipdb; ipdb.set_trace()
-
-    os.system(f"rm -rf {results_path}")
-    os.system(f"rm -rf {analysis_path}")
+    #os.system(f"rm -rf {runner.config.main_path}")
